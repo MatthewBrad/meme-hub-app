ebd504133abdd8ffd21f3fb02791358060d9ce97
Matthew <matthewbradley@hotmail.co.za>
2023-09-19 17:24:28 +0200

    Base project set-up, repo created, Vue project created using Vue CLI. Pinia, Bootstrap 5 and SASS have been added to the project.

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul class="nav justify-content-between p-3">
+      <li class="nav-item">
+        <img alt="Reflective Learning logo" src="../assets/rl-icon.png" />
+      </li>
+      <li class="nav-item">
+        <h1>MemeHub</h1>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar-Component",
+};
+</script>
+
+<style lang="sass" scoped>
+img
+  max-width: 150px
+</style>
